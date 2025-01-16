3c98d152b1ce3ca49f208d045368b018f2557bd4
isepLapa <1241607@isep.ipp.pt>
2025-01-16 20:36:33 +0000

    Fix de alguns problemas, adicionado validaçoes e adicionado salvar para ficheiro

--- src/Gerenciamento/Utentes.java
+++ src/Gerenciamento/Utentes.java
@@ -1,15 +1,30 @@
 package Gerenciamento;
 
+import Storage.Storage;
 import Utils.Utils;
 
+import java.nio.file.Path;
 import java.util.ArrayList;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Utentes {
-    Scanner sc = new Scanner(System.in);
+    private final Biblioteca biblioteca;
+    private final Storage storage;
 
-    private ArrayList<Utente> utentes = new ArrayList<>();
+    private final Path utentesPath;
+
+    Scanner sc = new Scanner(System.in);
+    private List<Utente> utentes = new ArrayList<>();
+
+    public Utentes(Biblioteca biblioteca, String nomeBiblioteca, Storage storage) {
+        this.biblioteca = biblioteca;
+        this.storage = storage;
+
+        this.utentesPath = Path.of(nomeBiblioteca + "/utentes.txt");
+        this.utentes = this.getUtentes();
+    }
 
     public void removerUtente(){
         System.out.println("------------------------------------");
@@ -19,6 +34,7 @@ public class Utentes {
         for (Utente utenteObj : utentes) {
             if (utenteObj.getNif().equals(nifTemp)) {
                 utentes.remove(utenteObj);
+                this.storage.save(this.utentesPath, utentes);
                 System.out.println(utenteObj.toString() + "\n" +"Removido com sucesso");
                 break;
             }
@@ -29,12 +45,14 @@ public class Utentes {
     }
 
     public void adicionarUtente() {
-        String nif = Utils.ScanString("introduza o nif");
-        String name = Utils.ScanString("introduza o nome");
-        String gender = Utils.ScanString("introduza o genero");
-        String contact = Utils.ScanString("introduza o Contacto");
+        String nif = Utils.validaNif("Introduza o nif: ");
+        String name = Utils.ScanString("Introduza o nome: ");
+        String gender = Utils.ScanString("Introduza o genero: ");
+        String contact = Utils.ScanString("Introduza o Contacto: ");
         Utente utente = new Utente(nif, name, gender, contact);
         utentes.add(utente);
+
+        this.storage.save(this.utentesPath, utentes);
     }
 
     public void listarUtentes() {
@@ -61,6 +79,37 @@ public boolean verificarNifUtentesNaLista(String nif) {
 
     return false;
 }
+
+    public List<Utente> getUtentes() {
+        if(utentes.isEmpty()){
+            return this.convertToUtenteList(storage.get(this.utentesPath));
+        }
+
+        return utentes;
+    }
+
+    /**
+     * Converte uma lista de strings em uma lista de objetos Livro.
+     *
+     * @param utentesStr a lista de strings dos utentes
+     * @return uma lista de objetos Livro
+     */
+    public List<Utente> convertToUtenteList(List<String> utentesStr) {
+        List<Utente> utentes = new ArrayList<>();
+        for (String livroStr : utentesStr) {
+            utentes.add(parseUtente(livroStr));
+        }
+        return utentes;
+    }
+
+    private Utente parseUtente(String utente) {
+        String[] partesUtente = utente.split(" ");
+        String nif = partesUtente[0].split("=")[1];
+        String nome = partesUtente[1].split("=")[1];
+        String genero = partesUtente[2].split("=")[1];
+        String contacto = partesUtente[3].split("=")[1];
+        return new Utente(nif, nome, genero, contacto);
+    }
 
 }
 
